fab6620cb464272b099b1dd158515c36359eb363
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-02-04 13:49:43 +0100

    Finished Exercici 4 Closes #7

---
+++ MyUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4._Activitats
+{
+    public class MyUtils
+    {
+        public static int GetPositiveNum()
+        {
+            const string ErrorMessage = "El valor ha de ser un nombre positiu.";
+            if (!int.TryParse(Console.ReadLine(), out int value) || value <= 0)
+            {
+                throw new FormatException(ErrorMessage);
+            }
+            return value;
+        }
+    }
+}
