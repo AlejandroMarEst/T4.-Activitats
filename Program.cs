015fa03c7632b37b1c481ee83c47fce3654d86cf
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-02-06 13:55:18 +0100

    Finished Exercici 10 Closes #17

--- Program.cs
+++ Program.cs
@@ -8,31 +8,15 @@ namespace ExercicisTema4
     {
         public static void Main()
         {
-            const string Existeix = "Aquest nom existeix al diccionari";
-            const string NoExisteix = "Aquest nom no existeix al diccionari";
-            const string Edat = "L'edat de {0} es {1}";
-            string nameUser;
-            bool exists;
-            Dictionary<string, int> dictionaryNameAge = new Dictionary<string, int>();
-            dictionaryNameAge.Add("Marc", 21);
-            dictionaryNameAge.Add("Laura", 19);
-            dictionaryNameAge.Add("Pau", 22);
-            foreach (var item in dictionaryNameAge)
-            {
-                Console.WriteLine(item);
-            }
-            nameUser = Console.ReadLine();
-            exists = dictionaryNameAge.ContainsKey(nameUser);
-            Console.WriteLine(exists ? Existeix : NoExisteix);
-            if (exists)
-            {
-                Console.WriteLine(Edat, nameUser, dictionaryNameAge[nameUser]);
-            }
-            dictionaryNameAge.Remove("Laura");
-            foreach(var item in dictionaryNameAge)
-            {
-                Console.WriteLine(item);
-            }
+            const string InsertData = "Inserta una data en format DD,MM,YYYY";
+            const string Again = "Inserta una altre data";
+            const string Yes = "La segona data es més gran";
+            const string No = "La primera data es més gran";
+            Console.WriteLine(InsertData);
+            DateTime dataUser1 = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine(Again);
+            DateTime dataUser2 = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine(MyUtils.CheckFutureDate(dataUser1,dataUser2)?Yes:No);
         }
     }
 }
