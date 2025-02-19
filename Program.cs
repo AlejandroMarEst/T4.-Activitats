100c462a40820f80c272b668b14760b9b30ad4e7
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-02-19 10:13:55 +0100

    Finished Exercici 19 Closes #35

--- Program.cs
+++ Program.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections;
 using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
 using T4._Activitats;
 namespace ExercicisTema4
 {
     public class Program
     {
+        public delegate int DelegateEx19(int num1, int num2);
+        public static int Multiplicar(int num1, int num2) => num1 * num2;
+        public static int Dividir(int num1, int num2) => num1 / num2;
         public static void Main()
         {
-            List<int> list = new List<int> { 10, 20, 849489, 87, 455, 512, 100 };
-            list = list.Where(x => x < 500).ToList();
-            foreach (int item in list) {
-                Console.WriteLine(item);
-            }
+            DelegateEx19 delegat = Multiplicar;
+            Console.WriteLine(delegat(20, 2));
+            delegat = Dividir;
+            Console.WriteLine(delegat(20, 2));
         }
     }
 }
