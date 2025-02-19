81e9a0c183a0e87be44bca8d647ede12c49ff276
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-02-19 10:21:31 +0100

    Finished Exercici 20 Closes #37

--- Program.cs
+++ Program.cs
@@ -8,14 +8,18 @@ namespace ExercicisTema4
     public class Program
     {
         public delegate int DelegateEx19(int num1, int num2);
+        public static int Sumar(int num1, int num2) => num1 + num2;
         public static int Multiplicar(int num1, int num2) => num1 * num2;
         public static int Dividir(int num1, int num2) => num1 / num2;
+        public static int ExecutarOperacio(int num1, int num2, DelegateEx19 delegat) => delegat(num1, num2);
         public static void Main()
         {
-            DelegateEx19 delegat = Multiplicar;
-            Console.WriteLine(delegat(20, 2));
-            delegat = Dividir;
-            Console.WriteLine(delegat(20, 2));
+            const int num1 = 20;
+            const int num2 = 2;
+            Console.WriteLine(ExecutarOperacio(num1, num2, Multiplicar));
+            Console.WriteLine(ExecutarOperacio(num1, num2, Dividir));
+            Console.WriteLine(ExecutarOperacio(num1, num2, Sumar));
+
         }
     }
 }
