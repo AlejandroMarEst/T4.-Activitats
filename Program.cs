1995e0d54ab36f92c4f3bd319e69a1c5ebb8efdb
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-02-19 10:27:28 +0100

    Finished Exercici 21 Closes #39

--- Program.cs
+++ Program.cs
@@ -7,19 +7,21 @@ namespace ExercicisTema4
 {
     public class Program
     {
-        public delegate int DelegateEx19(int num1, int num2);
-        public static int Sumar(int num1, int num2) => num1 + num2;
-        public static int Multiplicar(int num1, int num2) => num1 * num2;
-        public static int Dividir(int num1, int num2) => num1 / num2;
-        public static int ExecutarOperacio(int num1, int num2, DelegateEx19 delegat) => delegat(num1, num2);
+        public delegate void DelegateEx(string msg);
+        public static void WriteMSG(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        public static void WriteStarryMSG(string msg)
+        {
+            Console.WriteLine("*" + msg + "*");
+        }
         public static void Main()
         {
-            const int num1 = 20;
-            const int num2 = 2;
-            Console.WriteLine(ExecutarOperacio(num1, num2, Multiplicar));
-            Console.WriteLine(ExecutarOperacio(num1, num2, Dividir));
-            Console.WriteLine(ExecutarOperacio(num1, num2, Sumar));
-
+            const string Text = "Hola, bon dia";
+            DelegateEx delegat = WriteMSG;
+            delegat += WriteStarryMSG;
+            delegat(Text);
         }
     }
 }
